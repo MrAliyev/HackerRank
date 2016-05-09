1beacb128fcd0cb353c15ba570525cab9eb2cb24
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-09 17:23:40 +0500

    solved

--- 30 Days of Code/day12.php
+++ 30 Days of Code/day12.php
@@ -69,9 +69,9 @@ class Person {
 
     // Constructor
     Person(String firstName, String lastName, int identification){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.idNumber = identification;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
     }
 
     // Print person data
@@ -120,19 +120,19 @@ class Student extends Person{
 
 class Solution {
     public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            String firstName = scan.next();
-            String lastName = scan.next();
-            int id = scan.nextInt();
-            int numScores = scan.nextInt();
-            int[] testScores = new int[numScores];
-            for(int i = 0; i < numScores; i++){
-                testScores[i] = scan.nextInt();
-            }
-            scan.close();
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for(int i = 0; i < numScores; i++){
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
 
-            Student s = new Student(firstName, lastName, id, testScores);
-            s.printPerson();
-            System.out.println("Grade: " + s.calculate() );
+        Student s = new Student(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate() );
     }
 }

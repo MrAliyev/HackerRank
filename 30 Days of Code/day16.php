28aa41c8fb3e145b817d4ee23e7ddfc3df83d907
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-13 16:44:37 +0500

    solved

--- 30 Days of Code/day16.php
+++ 30 Days of Code/day16.php
@@ -17,6 +17,7 @@ Constraints
 
 1<= |S| <= 6, where |S| is the length of string S.
 S is composed of either lowercase letters (a-z) or decimal digits (0-9).
+
 Output Format
 
 Print the parsed integer value of S, or Bad String if S cannot be converted to an integer.

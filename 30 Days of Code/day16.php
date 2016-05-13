d3315afc99e95693c8454d83bc73641651a43e6e
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-13 16:43:18 +0500

    solved

--- 30 Days of Code/day16.php
+++ 30 Days of Code/day16.php
@@ -1,8 +1,44 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Objective 
+Today, we're getting started with Exceptions by learning how to parse an integer from a string and print a custom error message.
+
+Task 
+Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+
+Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get a 0 score.
+
+Input Format
+
+A single string, S.
+
+Constraints
+
+1<= |S| <= 6, where |S| is the length of string S.
+S is composed of either lowercase letters (a-z) or decimal digits (0-9).
+Output Format
+
+Print the parsed integer value of S, or Bad String if S cannot be converted to an integer.
  */
 
+
+// JAVA
+// Needs a hack to complete in PHP
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        try{
+            int num = Integer.parseInt(s);
+            System.out.println(num);
+        }catch(Exception e){
+            System.out.println("Bad String");
+        }
+    }
+}

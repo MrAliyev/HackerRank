134a3a9d10a8073a237011c0345a7a7af139fc22
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-14 13:53:26 +0500

    solved

--- 30 Days of Code/day17.php
+++ 30 Days of Code/day17.php
@@ -1,8 +1,22 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Objective
+  Yesterday's challenge taught you to manage exceptional situations by using try and catch blocks. In today's challenge, you're going to practice throwing and propagating an exception.
+
+  Task
+  Write a Calculator class with a single method: int power(int,int). The power method takes two integers, n and p, as parameters and returns the integer result of n^p. If either n or p is negative, then the method must throw an exception with the message: n and p should be non-negative.
+
+  Note: Do not use an access modifier (e.g.: public) in the declaration for your Calculator class.
  */
 
+class Calculator {
+
+    public function power($n, $p) {
+        if ($n < 0 or $p < 0) {
+            throw new Exception('n and p should be non-negative');
+        }
+        return pow($n, $p);
+    }
+
+}

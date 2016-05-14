981c2ac2d55db51ad2b19c38e1a7ab652b52278f
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-14 14:13:55 +0500

    solved 0-17

--- 30 Days of Code/day17-More Exceptions.php
+++ 30 Days of Code/day17-More Exceptions.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
   Objective
   Yesterday's challenge taught you to manage exceptional situations by using try and catch blocks. In today's challenge, you're going to practice throwing and propagating an exception.

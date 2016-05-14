56eb8dcccda676cb356e85b1ea3b6c6fad7e748e
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-14 13:53:56 +0500

    solved

--- 30 Days of Code/day17.php
+++ 30 Days of Code/day17.php
@@ -20,3 +20,15 @@ class Calculator {
     }
 
 }
+
+$myCalculator = new Calculator();
+$T = intval(fgets(STDIN));
+while ($T-- > 0) {
+    list($n, $p) = explode(" ", trim(fgets(STDIN)));
+    try {
+        $ans = $myCalculator->power($n, $p);
+        echo $ans . "\n";
+    } catch (Exception $e) {
+        echo $e->getMessage() . "\n";
+    }
+}

75a8663692b6dcef5c12081dcddb7f723a5d6b17
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-15 13:31:31 +0500

    solved 0-18

--- 30 Days of Code/day18-Queues and Stacks.php
+++ 30 Days of Code/day18-Queues and Stacks.php
@@ -1,8 +1,65 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Welcome to Day 18! Today we're learning about Stacks and Queues.
+
+  A palindrome is a word, phrase, number, or other sequence of characters which reads the same backwards and forwards. Can you determine if a given string, s, is a palindrome?
+
+  To solve this challenge, we must first take each character in s, enqueue it in a queue, and also push that same character onto a stack. Once that's done, we must dequeue the first character from the queue and pop the top character off the stack, then compare the two characters to see if they are the same; as long as the characters match, we continue dequeueing, popping, and comparing each character until our containers are empty (a non-match means s isn't a palindrome).
  */
 
+class Solution {
+
+    private $stack = [];
+    private $queue = [];
+    
+    public function pushCharacter($char){
+        $this->stack[] = $char;
+    }
+    
+    public function enqueueCharacter($char){
+        $this->queue[] = $char;
+    }
+    
+    public function popCharacter(){
+        return array_pop($this->stack);
+    }
+        
+    public function dequeueCharacter(){
+        return array_shift($this->queue);
+    }
+
+}
+
+// read the string s
+$s = fgets(STDIN);
+
+$obj = new Solution();
+
+$len = strlen($s);
+$isPalindrome = true;
+
+// push/enqueue all the characters of string s to stack
+for ($i = 0; $i < $len; $i++) {
+    $obj->pushCharacter($s{$i});
+    $obj->enqueueCharacter($s{$i});
+}
+
+/*
+  pop the top character from stack
+  dequeue the first character from queue
+  compare both the characters
+ */
+for ($i = 0; $i < $len / 2; $i++) {
+    if ($obj->popCharacter() != $obj->dequeueCharacter()) {
+        $isPalindrome = false;
+
+        break;
+    }
+}
+
+if ($isPalindrome) {
+    echo "The word, " . $s . ", is a palindrome.";
+} else {
+    echo "The word, " . $s . ", is not a palindrome.";
+}

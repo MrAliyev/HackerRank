4a174897f3f6e37f3ca1f2af878a15789755dec1
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-17 21:00:38 +0500

    solved 0 - 20

--- 30 Days of Code/day20-Sorting.php
+++ 30 Days of Code/day20-Sorting.php
@@ -1,8 +1,40 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+Objective 
+Today, we're discussing a simple sorting algorithm called Bubble Sort.
+
+Task 
+Given an array, a, of size n containing distinct elements a[0], a[1],...,a[n-1], sort array a in ascending order using the Bubble Sort algorithm. 
+Once sorted, print the following 3 lines:
+
+1. Array is sorted in $numberOfSwaps swaps
+where $numberOfSwaps is the number of swaps that took place.
+2. First Element: $firstElement
+where $firstElement is the first element in the sorted array.
+3. Last Element: $LastElement
+where $LastElement is the last element in the sorted array.
  */
 
+$handle = fopen("php://stdin", "r");
+fscanf($handle, "%d", $n);
+$a_temp = fgets($handle);
+$a = explode(" ", $a_temp);
+array_walk($a, 'intval');
+
+$numberOfSwaps = 0;
+
+for ($i = 0; $i < $n; $i++) {
+    for ($j = 0; $j < $n - 1; $j++) {
+        if ($a[$j] > $a[$j + 1]) {
+            $tmp = $a[$j];
+            $a[$j] = $a[$j + 1];
+            $a[$j + 1] = $tmp;
+            $numberOfSwaps++;
+        }
+    }
+}
+
+print "Array is sorted in $numberOfSwaps swaps.\n";
+print "First Element: $a[0]\n";
+print "Last Element: {$a[count($a) - 1]}\n";

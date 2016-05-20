494d22c5cf93b67a1c7f1939688e1c41421b5c84
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-20 19:53:01 +0500

    solved 0 - 23

--- 30 Days of Code/day23-BST Level-Order Traversal.php
+++ 30 Days of Code/day23-BST Level-Order Traversal.php
@@ -1,8 +1,65 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Objective
+  Today, we're going further with Binary Search Trees.
+
+  Task
+  A level-order traversal, also known as a breadth-first search, visits each level of a tree's nodes from left to right, top to bottom. You are given a pointer, root, pointing to the root of a binary search tree. Complete the levelOrder function provided in your editor so that it prints the level-order traversal of the binary search tree.
  */
 
+class Node {
+
+    public $left, $right;
+    public $data;
+
+    function __construct($data) {
+        $this->left = $this->right = null;
+        $this->data = $data;
+    }
+
+}
+
+class Solution {
+
+    public function insert($root, $data) {
+        if ($root == null) {
+            return new Node($data);
+        } else {
+            if ($data <= $root->data) {
+                $cur = $this->insert($root->left, $data);
+                $root->left = $cur;
+            } else {
+                $cur = $this->insert($root->right, $data);
+                $root->right = $cur;
+            }
+            return $root;
+        }
+    }
+
+    public function levelOrder($root) {
+
+        $nodes = [$root];
+
+        while (count($nodes) > 0) {
+            $node = array_shift($nodes);
+            if ($node->left) {
+                $nodes[] = $node->left;
+            }
+            if ($node->right) {
+                $nodes[] = $node->right;
+            }
+            print $node->data . " ";
+        }
+    }
+
+}
+
+$myTree = new Solution();
+$root = null;
+$T = intval(fgets(STDIN));
+while ($T-- > 0) {
+    $data = intval(fgets(STDIN));
+    $root = $myTree->insert($root, $data);
+}
+$myTree->levelOrder($root);

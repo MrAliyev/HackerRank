2174ec53a4ed4f3091af1e5824a0fe183304c2cd
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-22 15:38:41 +0500

    solved 0 - 25

--- 30 Days of Code/day25-Running Time and Complexity.php
+++ 30 Days of Code/day25-Running Time and Complexity.php
@@ -1,8 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Task
+  A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself. Given a number, n, determine and print whether it's Prime or Not prime.
  */
 
+$_fp = fopen("php://stdin", "r");
+$d = fgets($_fp);
+
+while ($n = fgets($_fp)) {
+    echo isPrime($n);
+}
+
+function isPrime($n) {
+    if ($n == 2 || $n == 3) {
+        return "Prime\n";
+    }
+    if ($n == 1 || $n % 2 == 0 || $n % 3 == 0) {
+        return "Not prime\n";
+    }
+    $i = 5;
+
+    while ($i * $i <= $n) {
+        if ($n % $i == 0 || $n % ($i + 2) == 0) {
+            return "Not prime\n";
+        }
+        $i+=6;
+    }
+    return "Prime\n";
+}

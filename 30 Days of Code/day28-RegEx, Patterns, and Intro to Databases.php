4fd425990afdb6b355fdb5ffcf43518579de8fc1
Emin Aliyev <emin.aliyev@gmail.com>
2016-05-25 14:54:26 +0500

    solved 0 - 28

--- 30 Days of Code/day28-RegEx, Patterns, and Intro to Databases.php
+++ 30 Days of Code/day28-RegEx, Patterns, and Intro to Databases.php
@@ -1,8 +1,32 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+  Objective
+  Today, we're working with regular expressions.
+
+  Task
+  Consider a database table, Emails, which has the attributes First Name and Email ID.
+  Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people
+  whose email address ends in @gmail.com.
+
+  Input Format
+
+  The first line contains an integer, N, total number of rows in the table.
+  Each of the N subsequent lines contains 2 space-separated strings denoting a person's first name and email ID, respectively.
+
+
  */
 
+$handle = fopen("php://stdin", "r");
+fscanf($handle, "%d", $N);
+for ($a0 = 0; $a0 < $N; $a0++) {
+    //fscanf($handle,"%s %s",$firstName,$emailID);
+    preg_match("/(\w+)(.+@gmail\.com)/", fgets($handle), $matches[]);
+}
+
+sort($matches);
+
+foreach ($matches as $name) {
+    if (!empty($name))
+        print $name[1] . "\n";
+}
